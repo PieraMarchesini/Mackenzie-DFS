919d73d11b26426aa1c30355b32545f2b6c576a3
Piera Marchesini <pieracarolina@yahoo.com.br>
2017-11-18 15:05:49 -0200

    DFS implementation finished

--- DFS/DFS/main.c
+++ DFS/DFS/main.c
@@ -1,3 +1,11 @@
+/*
+ Autores:
+ Piera Carolina Marchesini Dutra - TIA: 31581773
+ Ana Carolina dos Santos Silva - TIA:
+ Michelle Faifer dos Santos - TIA:
+ Priscilla - TIA:
+ */
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <memory.h>
@@ -18,13 +26,16 @@ typedef struct vert{
     int name;
     int color;
     int predecessor;
-    int dist;
+    int d;
+    int f;
     Edge *first;
 }Vertex;
 
-void printGraphDistances(Vertex G[], int order);
+int varTime;
+
+void printGraphProperties(Vertex G[], int order);
 void printGraph(Vertex G[], int order);
-void createGraph(Vertex **G, int order);
+void createDigraph(Vertex **G, int order);
 void addEdge(Vertex G[], int order, int v1, int v2);
 int calcSize(Vertex G[], int order);
 void dfs(Vertex G[], int order);
@@ -51,7 +62,7 @@ int isEmpty(Queue f){
 void enqueue(Queue *f, int elem);
 int dequeue(Queue *f);
 
-void createGraph(Vertex **G, int order){
+void createDigraph(Vertex **G, int order){
     int i;
     *G= (Vertex*) malloc(sizeof(Vertex)*order);
     
@@ -59,7 +70,8 @@ void createGraph(Vertex **G, int order){
         (*G)[i].name= i;
         (*G)[i].color= NONE;
         (*G)[i].predecessor= -1;
-        (*G)[i].dist= 999;
+        (*G)[i].d = -1;
+        (*G)[i].f = -1;
         (*G)[i].first= NULL;
     }
 }
@@ -84,7 +96,7 @@ void dfs(Vertex G[], int order){
         G[i].color = WHITE;
         G[i].predecessor = -1;
     }
-    
+    varTime = 0;
     for (i=0; i<order; i++){
         if (G[i].color == WHITE) {
             dfsVisit(G, i);
@@ -95,19 +107,20 @@ void dfs(Vertex G[], int order){
 void dfsVisit(Vertex G[], int v){
     int j;
     G[v].color = GRAY;
+    varTime += 1;
+    G[v].d = varTime;
     
     Edge *aux = G[v].first;
     if (aux != NULL)
-        printf("first: %d\n", aux->name);
     for(j=0; aux != NULL; aux= aux->prox, j++){
-        printf("G[j]: %d\n", G[j].name);
         if (G[aux->name].color == WHITE) {
-            printf("G[j] %d\n", G[aux->name].name);
             G[aux->name].predecessor = v;
             dfsVisit(G, j);
         }
     }
     G[v].color = BLACK;
+    varTime += 1;
+    G[v].f = varTime;
 }
 
 int calcSize(Vertex G[], int order){
@@ -135,31 +148,25 @@ void printGraph(Vertex G[], int order){
     printf("\n\n");
 }
 
-void printGraphDistances(Vertex G[], int order){
+void printGraphProperties(Vertex G[], int order){
     int i;
+    printf("- Vertex number\n");
     for(i=0; i<order; i++)
         printf("%3d ",G[i].name);
     printf("\n");
+    printf("- d\n");
     for(i=0; i<order; i++)
-        printf("%3d ",G[i].dist);
+        printf("%3d ",G[i].d);
     printf("\n");
+    printf("- f (f > d)\n");
+    for(i=0; i<order; i++)
+        printf("%3d ",G[i].f);
+    printf("\n");
+    printf("- Color (2 means black, that is the examine of vertex was finished)\n");
     for(i=0; i<order; i++)
         printf("%3d ",G[i].color);
     printf("\n");
-    for(i=0; i<order; i++)
-        printf("%3d ",G[i].predecessor);
-    printf("\n");
-    
-}
-
-void printGraphColors(Vertex G[], int order){
-    int i;
-    for(i=0; i<order; i++)
-        printf("%3d ",G[i].name);
-    printf("\n");
-    for(i=0; i<order; i++)
-        printf("%3d ",G[i].dist);
-    printf("\n");
+    printf("- Predecessor (-1 means that the vertex doesn't have a predecesor)\n");
     for(i=0; i<order; i++)
         printf("%3d ",G[i].predecessor);
     printf("\n");
@@ -173,21 +180,22 @@ int main(int argc, char *argv[]) {
     
     Queue f;
     initializeEmpty(&f);
-    if (isEmpty(f))
-        printf("\nFila esta vazia");
     
-    createGraph(&G, orderG);
+    createDigraph(&G, orderG);
     addEdge(G,orderG,3,4);
     addEdge(G,orderG,4,2);
     addEdge(G,orderG,5,4);
     addEdge(G,orderG,2,3);
     addEdge(G,orderG,3,7);
     
-    printf("\nTamanho: %d\n",calcSize(G, orderG));
-    
+    printf("\nSize of created digraph: %d\n\n",calcSize(G, orderG));
+    printf("Adjacency list of digraph");
     printGraph(G, orderG);
+    
     dfs(G, orderG);
-    printGraphDistances(G, orderG);
+    
+    printf("Properties of digraph after the DFS\n");
+    printGraphProperties(G, orderG);
     
     printf("\n");
     
